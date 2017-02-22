11c60768b61e652556b92fc87c510bebbce81952
Daniel Weibel <danielmweibel@gmail.com>
2017-02-22 22:10:16 +0700

    Display data retrieved from API in RecyclerView
    Display thumbnail and article title of the retrieved 10 articles
    in RecyclerView with a GridLayoutManager. The search term can be
    entered into an EditText and clicking on a button next to it
    triggers the API call.

--- app/src/main/java/org/weibeld/nytexplore/MainActivity.java
+++ app/src/main/java/org/weibeld/nytexplore/MainActivity.java
@@ -4,13 +4,23 @@ import android.app.Activity;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+
+import org.weibeld.nytexplore.api.ApiService;
 import org.weibeld.nytexplore.api.ApiServiceSingleton;
 import org.weibeld.nytexplore.databinding.ActivityMainBinding;
 import org.weibeld.nytexplore.model.ApiResponse;
 import org.weibeld.nytexplore.model.Doc;
+import org.weibeld.nytexplore.model.Multimedium;
 
 import java.util.ArrayList;
 
@@ -25,12 +35,27 @@ public class MainActivity extends AppCompatActivity {
     Activity mActivity;
     ActivityMainBinding b;
 
+    ArrayList<Doc> mArticles;
+    ArticleAdapter mAdapter;
+    RecyclerView.LayoutManager mLayoutManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         b = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
         mActivity = this;
+
+        mArticles = new ArrayList<Doc>();
+
+        // Set adapter for the RecyclerView
+        mAdapter = new ArticleAdapter(mArticles);
+        b.recyclerView.setAdapter(mAdapter);
+
+        // Set LayoutManager for the RecyclerView
+        mLayoutManager = new GridLayoutManager(this, 4, GridLayoutManager.VERTICAL, false);
+        //mLayoutManager = new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL);
+        b.recyclerView.setLayoutManager(mLayoutManager);
 
         b.btnFind.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,10 +65,13 @@ public class MainActivity extends AppCompatActivity {
                 call.enqueue(new Callback<ApiResponse>() {
                     @Override
                     public void onResponse(Call<ApiResponse> call, Response<ApiResponse> response) {
-                        ArrayList<Doc> docs = (ArrayList<Doc>) response.body().getResponse().getDocs();
-                        for (Doc doc : docs) {
-                            if (doc.getSnippet() != null)
-                                Log.d(LOG_TAG, doc.getSnippet());
+                        ArrayList<Doc> articles = (ArrayList<Doc>) response.body().getResponse().getDocs();
+                        mArticles.clear();
+                        mArticles.addAll(articles);
+                        mAdapter.notifyDataSetChanged();
+                        for (Doc doc : mArticles) {
+                            if (doc.getHeadline() != null && doc.getHeadline().getMain() != null)
+                                Log.d(LOG_TAG, doc.getHeadline().getMain());
                         }
                     }
                     @Override
@@ -52,8 +80,72 @@ public class MainActivity extends AppCompatActivity {
                 });
             }
         });
+    }
 
 
+
+    public class ArticleAdapter extends RecyclerView.Adapter<ArticleAdapter.ViewHolder> {
+
+        private ArrayList<Doc> mData;
+
+        public ArticleAdapter(ArrayList<Doc> data) {
+            mData = data;
+        }
+
+        // Called when a new view for an item must be created. This method does not return the view of
+        // the item, but a ViewHolder, which holds references to all the elements of the view.
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            // The view for the item
+            View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_article, parent, false);
+            // Create a ViewHolder for this view and return it
+            return new ViewHolder(itemView);
+        }
+
+        // Populate the elements of the passed view (represented by the ViewHolder) with the data of
+        // the item at the specified position.
+        @Override
+        public void onBindViewHolder(ViewHolder vh, int position) {
+            Doc article = mData.get(position);
+
+            vh.tvTitle.setText(getSafeString(article.getHeadline().getMain()));
+
+            ArrayList<Multimedium> multimedia = (ArrayList<Multimedium>) article.getMultimedia();
+            String thumbUrl = "";
+            for (Multimedium m : multimedia) {
+                if (m.getType().equals("image") && m.getSubtype().equals("thumbnail")) {
+                    thumbUrl = ApiService.API_IMAGE_BASE_URL + m.getUrl();
+                    break;
+                }
+            }
+            Glide.with(mActivity).load(thumbUrl).into(vh.ivThumb);
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return mData.size();
+        }
+
+        private String getSafeString(String str) {
+            if (str == null)
+                return "";
+            else
+                return str;
+        }
+
+
+        public class ViewHolder extends RecyclerView.ViewHolder {
+            public ImageView ivThumb;
+            public TextView tvTitle;
+
+            // Create a viewHolder for the passed view (item view)
+            public ViewHolder(View view) {
+                super(view);
+                ivThumb = (ImageView) view.findViewById(R.id.ivThumb);
+                tvTitle = (TextView) view.findViewById(R.id.tvTitle);
+            }
+        }
 
     }
 }
