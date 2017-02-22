cc8e7f1679dbbcc7de55e82ffff106b48a41a9fe
Daniel Weibel <danielmweibel@gmail.com>
2017-02-22 19:58:07 +0700

    Add dependencies for RecyclerView

--- app/src/main/java/org/weibeld/nytexplore/MainActivity.java
+++ app/src/main/java/org/weibeld/nytexplore/MainActivity.java
@@ -5,6 +5,17 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    // NYTimes Article Search API
+    // Documentation:
+    //   - http://developer.nytimes.com/article_search_v2.json
+    //   - https://github.com/NYTimes/public_api_specs/tree/master/article_search
+    // NYT article search API returns maximum 10 results at a time (page 0 = 1-10, page 1 = 11-20, etc.)
+    // The highest page number that can be requested is 120
+    private static final String API_KEY = "923ff50fda8a4f10befc9489619ea1c3";  // GET query parameter api-key
+    private static final String API_BASE_URL = "https://api.nytimes.com/svc/search/v2/";
+    private static final String API_IMAGE_BASE_URL = "http://www.nytimes.com/";
+    private static final String API_CALL = "articlesearch.json";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
